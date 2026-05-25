ee637c931336c010ce7befa71b35923f7e9fb3f4
matias-leiva-4 <matias.leiva.b@usach.cl>
2026-05-24 20:33:50 -0400

    Fix Keycloak: remove onLoad check-sso + realm redirectUris use port 8070

--- travel-agency/src/main.jsx
+++ travel-agency/src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 createRoot(document.getElementById("root")).render(
   <ReactKeycloakProvider
     authClient={keycloak}
-    initOptions={{ onLoad: "check-sso", pkceMethod: "S256", checkLoginIframe: false }}
+    initOptions={{ pkceMethod: "S256", checkLoginIframe: false }}
   >
     <App />
   </ReactKeycloakProvider>,
